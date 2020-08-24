c923a3e363d7c3f52678afb274b2f1fab501e4c4
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2020-08-24 19:52:02 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a[5];
+
+    a[0] = 10;
+    a[1] = 20;
+    a[2] = 30;
+    a[3] = 40;
+    a[4] = 50;
+
+    printf("\n a[0] = %d", a[0]);
+    printf("\n a[1] = %d", a[1]);
+    printf("\n a[2] = %d", a[2]);
+    printf("\n a[3] = %d", a[3]);
+    printf("\n a[4] = %d", a[4]);
+
+    cin.get();
+    return 0;
+}
