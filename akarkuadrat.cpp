21558946c07bff90ca858461bb43a59187f0809e
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2020-08-24 19:51:31 +0700

    Add files via upload

---
+++ akarkuadrat.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    printf("\n sqrt(25) = %.2f", sqrt(25));
+    printf("\n sqrt(64) = %.2f", sqrt(64));
+    printf("\n sqrt(81) = %.2f", sqrt(81));
+
+    cin.get();
+    return 0;
+}
