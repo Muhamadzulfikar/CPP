cfaef3764b1a09db9a535250b57a8479c7a21fea
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-03-26 06:06:16 +0700

    Add files via upload

---
+++ oop siswa.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class siswa{
+	public:
+		string nama;
+		string tanggal_lahir;
+		string alamat;
+};
+
+int main(){
+	
+	siswa siswa01;
+	siswa01.nama = "Muhamad Zulfikar";
+	siswa01.tanggal_lahir = "19 Januari 2002";
+	siswa01.alamat = "Jl. Teluk Air, Kecematan karimun, Kabupaten Karimun";
+	
+	siswa siswa02;
+	siswa02.nama = "Putri Aulia Rahmah";
+	siswa02.tanggal_lahir ="01 Januari 2002";
+	siswa02.alamat ="Jl. Teluk Air, Kecamatan Karimun, Kabupaten Karimun";
+	
+	siswa siswa03;
+	siswa03.nama = "Irwansyah Saputra";
+	siswa03.tanggal_lahir;	
+	return 0;
+}
