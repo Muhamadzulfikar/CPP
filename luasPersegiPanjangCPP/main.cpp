c757739670e72724b9ff1b122c1af95eb571514b
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-03-27 12:09:35 +0700

    Luas Persegi Panjang

---
+++ luasPersegiPanjangCPP/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int panjang,lebar;
+	
+	cout<<"Masukan Panjang :";
+	cin>>panjang;
+	
+	cout<<"Masukan Lebar :";
+	cin>>lebar;
+	
+	cout<<"Luas Persegi Panjang :"<<panjang * lebar<<endl;
+	
+	system("pause");
+	return 0;
+}
