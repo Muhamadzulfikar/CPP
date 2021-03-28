7678a18b40d75497a89271c0d7b49eed5846aef2
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-03-28 08:30:57 +0700

    Luas Jajar Genjang

---
+++ luasJajarGenjang/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int alas,tinggi;
+	
+	cout<<"\t\t\t\tProgram menghitung luas jajar genjang"<<endl;
+	cout<<"\t\t\t\tCreate by Muhamad ZUlfikar 28/03/2021"<<endl;
+	
+	cout<<"\nMasukan alas : ";
+	cin>>alas;
+	cout<<"Masukan tinggi :";
+	cin>>tinggi;
+	
+	cout<<"\nLuas jajar genjang : "<<alas * tinggi<<endl;
+	
+	system("pause");
+	return 0;
+}
