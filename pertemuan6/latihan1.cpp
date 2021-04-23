6b81aa121919101c848430422a684382d126d7c0
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-04-23 16:50:42 +0700

    Add files via upload

---
+++ pertemuan6/latihan1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	//deklarasi array
+	int a[3] = {1, 0, 1};
+	
+	//menampilkan array
+	cout<<"Array ke-1 "<<a[0]<<endl;
+	cout<<"Array ke-2 "<<a[1]<<endl;
+	cout<<"array ke-3 "<<a[2]<<endl;
+	
+	system("pause");
+	
+	return 0;
+}
